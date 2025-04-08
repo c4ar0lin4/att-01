72d1267dd1adc0043c1f401cc8a26f9e3dc7cf82
c4ar0lin4 <car0lzinh4rc@gmail.com>
2025-04-08 18:33:09 +0000

    exercicio 6 finalizado

---
+++ 06.js
@@ -0,0 +1,12 @@
+function identificador(n1){
+if(n1 > 0){
+    console.log("positivo")
+}else if (numero < 0){
+    console.log("negativo");
+}else {
+    console.log("zero");
+}
+}
+verficarnumero(2);
+verficarnumero(-1);
+verficarnumero(0);
